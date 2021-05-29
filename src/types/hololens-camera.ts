6623c27ee3003c5707e1d59eb7853e74670a3700
drumath <2237k.drumath@gmail.com>
2021-05-30 01:43:48 +0900

    :sparkles: hololensのカメラの値

---
+++ src/types/hololens-camera.ts
@@ -0,0 +1,11 @@
+export default class CameraIntrinsics {
+  public static focalLength = {
+    x: 1153.877,
+    y: 1157.252,
+  };
+
+  public static principalOffset = {
+    x: 737.4859,
+    y: 401.8013,
+  };
+}
