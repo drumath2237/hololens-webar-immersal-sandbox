e58d835df0baddf2b24cddc17d6200d8e582bcf8
drumath <2237k.drumath@gmail.com>
2021-05-30 01:37:58 +0900

    :sparkles: immersal api用のinterfaceなどを整えた

---
+++ src/types/immersal-api.ts
@@ -0,0 +1,38 @@
+const immersalLocalizeURL = "https://api.immersal.com/localizeb64";
+
+interface SDKMapId {
+  id: number;
+}
+
+interface ImmersalLocalizeRequest {
+  token: string,
+  fx: number;
+  fy: number;
+  ox: number;
+  oy: number;
+  b64: string;
+  mapIds: SDKMapId[];
+}
+
+interface ImmersalLocalizeResponse {
+  success: boolean;
+  map: number;
+  px: number;
+  py: number;
+  pz: number;
+  r00: number;
+  r01: number;
+  r02: number;
+  r10: number;
+  r11: number;
+  r12: number;
+  r20: number;
+  r21: number;
+  r22: number;
+}
+
+export {
+  immersalLocalizeURL,
+  ImmersalLocalizeRequest,
+  ImmersalLocalizeResponse,
+};
