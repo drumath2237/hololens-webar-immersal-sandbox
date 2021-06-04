08fc4d7b4736f9f1b84d741d98e5fa1b4e6a68d8
drumath <2237k.drumath@gmail.com>
2021-06-04 16:30:11 +0900

    :sparkles: 不要なコメントを削除

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import {
   ArcRotateCamera,
   CannonJSPlugin,
   Color3,
-  DeviceOrientationCamera,
   Engine,
   HemisphericLight,
   Matrix,
@@ -50,13 +49,6 @@ window.addEventListener("DOMContentLoaded", async () => {
     );
     camera.attachControl();
     camera.minZ = 0.001;
-
-    // const camera = new DeviceOrientationCamera(
-    //   "dev-ori",
-    //   Vector3.Zero(),
-    //   scene
-    // );
-    // camera.minZ = 0.01;
 
     var light = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
     light.intensity = 0.5;
@@ -178,15 +170,6 @@ window.addEventListener("DOMContentLoaded", async () => {
     testCube.scaling.z *= 2;
     // testCube.isVisible = false;
 
-    testCube.onBeforeRenderObservable.add(() => {
-      // const direction = xr.baseExperience.camera
-      //   .getForwardRay()
-      //   .direction.clone();
-      // testCube.rotation.x = -direction.y;
-      // testCube.rotation.y = direction.x;
-      // const rot = xr.baseExperience.camera.rotationQuaternion.toEulerAngles();
-      // logTextBlock.text = `${rot}`;
-    });
 
     window.ondeviceorientation = (ev) => {
       if (ev.alpha && ev.beta && ev.gamma) {
@@ -211,85 +194,8 @@ window.addEventListener("DOMContentLoaded", async () => {
         mapIds: [{ id: Number(<string>process.env.MAP_ID) }],
       };
 
-      const positionWhenRequest = xr.baseExperience.camera.globalPosition;
-      const rotationWhenRequest = xr.baseExperience.camera.rotationQuaternion
-        .toEulerAngles()
-        .clone(); //cameraQuaternion.toEulerAngles();
-
       const cameraTransformMatrix = scene.activeCamera?.getWorldMatrix();
       if(!cameraTransformMatrix) return;
-
-      let rotMatrixWhenRequest = new Matrix(); //.RotationYawPitchRoll(
-      //   rotationWhenRequest.y,
-      //   rotationWhenRequest.x,
-      //   rotationWhenRequest.z
-      // );
-      Quaternion.FromEulerVector(rotationWhenRequest).toRotationMatrix(
-        rotMatrixWhenRequest
-      );
-
-      {
-        // ================ test with dummy data =================
-        // const res = {
-        //   error: "none",
-        //   success: true,
-        //   map: 1,
-        //   px: -0.82981,
-        //   py: 0.3349169,
-        //   pz: -1.38664,
-        //   r00: 0.33488,
-        //   r01: -0.437701,
-        //   r02: 0.834428,
-        //   r10: 0.9195104,
-        //   r11: -0.0041612,
-        //   r12: -0.390856,
-        //   r20: 0.205801,
-        //   r21: 0.898157,
-        //   r22: 0.388535,
-        // };
-        // const m = generateMatrixFromResponse(res);
-        // if (m) {
-        //   // testCube.position = new Vector3(res.px, res.py, res.pz); //.multiplyByFloats(0.1, 0.1, 0.1);
-        //   // testCube.rotation = Quaternion.FromRotationMatrix(m)
-        //   //   .conjugate()
-        //   //   .toEulerAngles();
-        //   // let rotQuaternion = Quaternion.Identity();
-        //   // m.decompose(undefined, rotQuaternion, undefined);
-        //   // const rotVec = rotQuaternion.conjugate().toEulerAngles(); //Quaternion.FromRotationMatrix(m).toEulerAngles();
-        //   // rootMesh.rotation = new Vector3(rotVec.z, rotVec.y, rotVec.x)
-        //   // rootMesh.rotate(new Vector3(0,0,1), Math.PI/2, Space.WORLD);
-        //   // rootMesh.position = new Vector3(res.px, res.py, res.pz).negate();
-        //   // rootMesh.setParent(testCube);
-        //   // const cubeMatrix = testCube.getWorldMatrix();
-        //   // // cubeMatrix.copyFrom(m.transpose());
-        //   // cubeMatrix.multiplyToRef(m.transpose(), cubeMatrix);
-        //   // const rootMeshMatrix = rootMesh.getPoseMatrix();
-        //   // rootMeshMatrix.multiplyToRef(m.transpose().invert(), rootMeshMatrix);
-        //   const scaleNagate = Matrix.FromValues(
-        //     -1,0,0,0,
-        //     0,-1,0,0,
-        //     0,0,-1,0,
-        //     0,0,0,1,
-        //   );
-        //   const transXY =Matrix.FromValues(
-        //     0,1,0,0,
-        //     -1,0,0,0,
-        //     0,0,1,0,
-        //     0,0,0,1
-        //   );
-        //   rootMesh.getDescendants().forEach((node)=>{
-        //     const maaa = node.getWorldMatrix();
-        //     // maaa.copyFrom(m.transpose().multiply(scaleNagate).invert());
-        //     // maaa.toggleModelMatrixHandInPlace();
-        //     maaa.multiplyToRef(m.transpose().invert().multiply(transXY), maaa);
-        //     const maaMat = (<Mesh>node).material;
-        //     if(maaMat){
-        //       maaMat.backFaceCulling = false;
-        //     }
-        //   })
-        // }
-        // ================ test with dummy data =================
-      }
 
       fetch(ImmersalAPI.immersalLocalizeURL, {
         method: "POST",
@@ -322,24 +228,7 @@ window.addEventListener("DOMContentLoaded", async () => {
             );
 
             rootMesh.getDescendants().forEach((node) => {
-              // const traNode = <TransformNode>node;
-              // if (!traNode) return;
-
               const worldMatrix = node.getWorldMatrix();
-
-              // traNode.setPivotPoint(Vector3.Zero(), Space.WORLD);
-
-              // const transformMatrix = Matrix.Translation(
-              //   positionWhenRequest.x,
-              //   positionWhenRequest.y,
-              //   positionWhenRequest.z
-              // );
-
-              // const negativeTransformMatrix = Matrix.Translation(
-              //   -positionWhenRequest.x,
-              //   -positionWhenRequest.y,
-              //   0
-              // );
 
               worldMatrix.multiplyToRef(
                 m
@@ -352,59 +241,17 @@ window.addEventListener("DOMContentLoaded", async () => {
                       0,0,-0.5
                     )
                   )
-                  // .multiply(
-                  //   Matrix.RotationX(Math.PI/24)
-                  // )
-                  // .multiply(
-                  //   Matrix.RotationZ(
-                  //     Math.PI/27
-                  //   )
-                  // )
-                  // .multiply(
-                  //   Matrix.RotationY(Math.PI/40)
-                  // )
                   .multiply(
                     Matrix.RotationYawPitchRoll(
                       Math.PI/30, Math.PI/21, 0
                     )
                   )
+                  // カメラ座標系→ワールド座標系への変換
                   .multiply(cameraTransformMatrix)
                   ,
                 worldMatrix
               );
 
-              // setTimeout(() => {
-              //   setInterval(() => {
-              //     const maa = Matrix.RotationY(Math.PI/12);
-              //     worldMatrix.multiplyToRef(maa, worldMatrix);
-              //   }, 1000);
-              // }, 1000);
-
-              // const maaa = node.getWorldMatrix();
-              // maaa.multiplyToRef(
-              //   m.transpose().invert().multiply(transXY),
-              //   // .multiply(
-              //   //   Matrix.Translation(
-              //   //     positionWhenRequest.x,
-              //   //     positionWhenRequest.y,
-              //   //     positionWhenRequest.z
-              //   //   )
-              //   // )
-              //   // .multiply(rotMatrixWhenRequest)
-              //   maaa
-              // );
-
-              // logTextBlock.text = `${rotMatrixWhenRequest.m.join("\n")}`;
-
-              // setTimeout(() => {
-              //   setInterval(() => {
-              //     traMaaaa.multiplyToRef(Matrix.RotationX(Math.PI / 12), traMaaaa);
-              //   }, 1000);
-              // }, 1000);
-              // rotMatrixWhenRequest.multiplyToRef(
-              //   maaa.multiply(m.transpose().invert().multiply(transXY)),
-              //   maaa
-              // );
             });
           }
         });
